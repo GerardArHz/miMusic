bf3921235dd6a2b3e951fb90870b23cd82548483
GerardArHz <gerardking961231@gmail.com>
2018-08-07 20:07:47 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+  appName: 'miMusica',
+  apiKey: 'f3ba8f9e1de03c14b8667d21d40af66a',
+  secret: '65ffab434aef3da8eba953cda71fae4f',
+  registeredTo: 'GerardArHz'
+}
+
+
+// Application name	miMusica
+// API key	f3ba8f9e1de03c14b8667d21d40af66a
+// Shared secret	65ffab434aef3da8eba953cda71fae4f
+// Registered to	GerardArHz
